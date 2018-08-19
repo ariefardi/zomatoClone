c39c8a714d971b80011bf9707b5b86386e549409
ariefardi <forzaariefpyon@gmail.com>
2018-08-19 13:33:25 +0700

    hit api zomato

--- ZomatoClone/src/components/LunchorDinnerRec/LunchorDinnerRec.js
+++ ZomatoClone/src/components/LunchorDinnerRec/LunchorDinnerRec.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Image, Text, StyleSheet, ScrollView, View, TouchableOpacity } from 'react-native'
+import {Image, Text, StyleSheet, ScrollView, View, TouchableOpacity, ActivityIndicator } from 'react-native'
 import { Card, CardItem, Left, Right } from 'native-base'
 import {connect} from 'react-redux'
 import {fetchingDataByCategory1} from '../../actions/zmAction'
@@ -11,40 +11,54 @@ class LunchorDinnerRec extends Component {
   }
   render() {
     const imageUrl = 'https://lh3.googleusercontent.com/zfLfmwlADOsxtrHqlqxscwRd215o6dk6EW-Voulxzv9FQuEYRTyQX1WlEct2AcJbFZw'
-    return (
-      <View>
-      <CardItem style={{backgroundColor: '#eaefeb'}} >
-            <Left> 
-                <Text 
-                style={{fontWeight:'700', color:'black'}} 
-                > 
-                Go out for lunch or dinner 
-                </Text>
-            </Left>
-            <Right>
-                <TouchableOpacity>
-                    <Text 
-                    style={{color:'#42f456', fontWeight:'bold'}} 
-                    > 
-                    See all 
-                    </Text>
-                </TouchableOpacity>
-            </Right>
-      </CardItem>
-      <ScrollView horizontal>
-          <Card style={styles.cardStyle} >
-            <CardItem cardBody>
-              <Image source={{uri: imageUrl }} style={{height: 90, width: null, flex: 1}}/>
-            </CardItem>
-                <View style={{marginTop:5}} >
-                  <Text style={{fontSize:15,fontWeight:'bold', color:'black'}} > Mie Yamin Jaly </Text>
-                  <Text style={{fontSize:13,marginTop:2}} > Ciledug, Tanggerang</Text>
-                  <Text style={{fontSize:11,marginTop:2}} > Bakmi, Aceh </Text>
-                </View>
-          </Card>
-      </ScrollView>
-      </View>
+    let card = (
+      <ActivityIndicator style={{alignItems: 'center', justifyContent: 'center', padding:8}} />
     )
+    if (this.props.item.length===0) {
+      console.log('masuk gak ke card kosong')
+      return card
+    }
+    else {
+      console.log(' ke render gak nih')
+      return (
+        <View>
+            <CardItem style={{backgroundColor: '#eaefeb'}} >
+                  <Left> 
+                      <Text 
+                      style={{fontWeight:'700', color:'black'}} 
+                      > 
+                      Go out for lunch or dinner 
+                      </Text>
+                  </Left>
+                  <Right>
+                      <TouchableOpacity>
+                          <Text 
+                          style={{color:'#42f456', fontWeight:'bold'}} 
+                          > 
+                          See all 
+                          </Text>
+                      </TouchableOpacity>
+                  </Right>
+            </CardItem>
+            <ScrollView horizontal>
+            {
+              this.props.item.map((item,index)=> (
+                <Card style={styles.cardStyle} key={index} >
+                  <CardItem cardBody>
+                    <Image source={{uri: item.restaurant['featured_image'] }} style={{height: 90, width: null, flex: 1}}/>
+                  </CardItem>
+                      <View style={{marginTop:5}} >
+                        <Text style={{fontSize:15,fontWeight:'bold', color:'black'}} > {item.restaurant.name} </Text>
+                        <Text style={{fontSize:13,marginTop:2}} > {item.restaurant.location['locality']} </Text>
+                        <Text style={{fontSize:11,marginTop:2}} > {item.restaurant.cuisines} </Text>
+                      </View>
+                </Card>
+              ))
+            }
+            </ScrollView>
+            </View>
+      )
+    }
   }
 }
 const styles = StyleSheet.create({
